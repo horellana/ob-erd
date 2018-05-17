9b169528b1f135420ca7c88f032c8b63917f71b0
Hector Orellana <pulga.p.29@gmail.com>
2018-05-17 15:52:46 -0400

    defcustom for imagemagick's convert

--- ob-erd.el
+++ ob-erd.el
@@ -5,6 +5,10 @@
   :group 'org-babel
   :type 'stirng)
 
+(defcustom org-imagemagick-convert-executable-path "convert"
+  "Path to imagemagick's convert executable."
+  :group 'org-babel
+  :type 'string)
 
 ;;;###autoload
 (defun org-babel-execute:erd (body params)
@@ -13,7 +17,17 @@ This function is called by `org-babel-execute-src-block'."
   (let* ((tmp-out-file (make-temp-file "ob-erd"))
          (tmp-file (make-temp-file "ob-erd"))
          (out-file (or (cdr (assq :file params))
-                       (error "erd requires a \":file\" header argument"))))
+                       (error "erd requires a \":file\" header argument")))
+
+         (erd-cmd (format "%s -i %s -o %s"
+                            org-erd-executable-path
+                            tmp-file
+                            tmp-out-file))
+
+         (convert-cmd (format "%s %s %s"
+                            org-imagemagick-convert-executable-path
+                            tmp-out-file
+                            out-file)))
 
     (org-babel-process-file-name tmp-file)
     (org-babel-process-file-name tmp-out-file)
@@ -22,13 +36,10 @@ This function is called by `org-babel-execute-src-block'."
     (with-temp-file tmp-file
       (insert body))
 
-    (org-babel-eval (format "%s -i %s -o %s"
-                            org-erd-executable-path
-                            tmp-file
-                            tmp-out-file)
-                    "")
+    (org-babel-eval erd-cmd "")
+    (org-babel-eval convert-cmd "")
 
-    (org-babel-eval (format "convert %s %s" tmp-out-file out-file) "")
     nil))
 
 (provide 'ob-erd)
+;;; ob-erd.el ends here
